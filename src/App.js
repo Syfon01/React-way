02797f72b2db6ddc6298810d8a601b99f3117abb
Sifon Isaac <syfonisaac@gmail.com>
2020-07-18 17:43:47 +0100

    faker conflict

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-
+import faker from 'faker'
 function App() {
   return (
     <div className="App ui container comments">
       <div className="comment">
         <div className="avatar">
-          <img src=" " alt="avatar" /></div>
+          <img src={faker.image.avatar()} alt="avatar" /></div>
         <div className="content">
-          <a className="author" href="/">Matt</a>
+          <a className="author">Matt</a>
           <div className="metadata"><div>Today at 5:42PM</div></div>
           <div className="text">How artistic!</div>
-          <div className="actions"><a className="" href="/">Reply</a></div>
         </div>
       </div>
     </div>
