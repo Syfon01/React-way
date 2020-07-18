1f9f751f59de0a5dca92c377ad865938b347dff5
Sifon Isaac <syfonisaac@gmail.com>
2020-07-18 17:25:07 +0100

    faker

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello there</h1>
+    <div className="App ui container comments">
+      <div className="comment">
+        <div className="avatar">
+          <img src=" " alt="avatar" /></div>
+        <div className="content">
+          <a className="author" href="/">Matt</a>
+          <div className="metadata"><div>Today at 5:42PM</div></div>
+          <div className="text">How artistic!</div>
+          <div className="actions"><a className="" href="/">Reply</a></div>
+        </div>
+      </div>
     </div>
   );
 }
