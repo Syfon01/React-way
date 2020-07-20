d7df5e00ad8b5b558be3cc9304b82c679a9c659e
Sifon Isaac <syfonisaac@gmail.com>
2020-07-20 22:29:04 +0100

    finally getting to understand props

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import CommentDetail from './components/CommentDetail'
 function App() {
   return (
     <div className="App ui container comments">
-      <CommentDetail />
+      <CommentDetail author="Sam" post="post one for today"/>
+      <CommentDetail author="Vee" post="Suuny day for learning react"/>
+      < CommentDetail author = "Merci"
+      post = "I can do better" />
+
     </div>
   );
 }
